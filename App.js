dfea4fb5c4d28d863c311aa53d8b96c8a8fc2c80
moknine <mokninemoknine999@gmail.com>
2024-03-03 19:57:59 +0100

    Last commit

--- App.js
+++ App.js
@@ -1,15 +1,52 @@
-import { StyleSheet, SafeAreaView, Platform, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  ScrollView,
+  FlatList,
+  Text,
+  View,
+  SectionList,
+} from "react-native";
 import PokiCard from "./components/PokiCard";
 import { data } from "./data";
+import dataGroup from "./data_group.json";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        {data.map((pokemon) => (
-          <PokiCard key={pokemon.name} {...pokemon} />
-        ))}
-      </ScrollView>
+      {/* <FlatList
+        data={data}
+        renderItem={({ item }) => {
+          return <PokiCard key={item.name} {...item} />;
+        }}
+        keyExtractor={(item) => item.name}
+        ItemSeparatorComponent={<View style={{ height: 16 }} />} //foraddmrgingYbetweenComponents
+        ListEmptyComponent={<Text style={styles.empty}>No items found</Text>}
+        ListHeaderComponent={
+          <Text style={styles.headerListText}>Pokemon List</Text>
+        }
+        ListFooterComponent={
+          <Text style={styles.footerListText}>End Of Pokemon List</Text>
+        }
+      /> */}
+      <View>
+        <SectionList
+          sections={dataGroup}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.card}>
+                <Text style={styles.cardText}>{item}</Text>
+              </View>
+            );
+          }}
+          renderSectionHeader={({ section }) => (
+            <Text style={styles.HeaderSectionText}>{section.type}</Text>
+          )}
+          ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+          SectionSeparatorComponent={() => <View style={{ height: 16 }} />}
+        />
+      </View>
     </SafeAreaView>
   );
 }
@@ -19,5 +56,40 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#f5f5f5",
     paddingTop: Platform.OS === "android" ? 25 : 0,
+    margin: 16,
+  },
+  // FlatList
+  empty: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+    textAlign: "center",
+    fontSize: 32,
+  },
+  headerListText: {
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 12,
+  },
+  footerListText: {
+    marginTop: 12,
+    fontSize: 24,
+    textAlign: "center",
+  },
+
+  // SectionList
+  card: {
+    padding: 16,
+    borderWidth: 2,
+    textAlign: "center",
+    borderRadius: 10,
+  },
+  cardText: {
+    textAlign: "center",
+    fontSize: 18,
+  },
+  HeaderSectionText: {
+    fontSize: 24,
+    fontWeight: "bold",
   },
 });
--- data.js
+++ data.js
@@ -1,33 +1,34 @@
-const chamanderData = {
-  name: "Chamander",
-  image: require("./assets/charmander.png"),
-  type: "Fire",
-  hp: 39,
-  moves: ["Scratch", "Ember", "Growl", "Leer"],
-  weaknesses: ["Water", "Rock"],
-};
-const squirtleData = {
-  name: "Squirtle",
-  image: require("./assets/squirtle.png"),
-  type: "Water",
-  hp: 44,
-  moves: ["Tackle", "Water Gun", "Tail", "Withdraw"],
-  weaknesses: ["Electric", "Grass"],
-};
-const bulbasaurData = {
-  name: "Bulbasaur",
-  image: require("./assets/bulbasaur.png"),
-  type: "Grass",
-  hp: 45,
-  moves: ["Tackle", "Vine Whipe", "Growl", "Leech Seed"],
-  weaknesses: ["Fire", "Ice", "Flaying", "Psychic"],
-};
-const pikatchuData = {
-  name: "Pikatchu",
-  image: require("./assets/pikachu.png"),
-  type: "Electric",
-  hp: 35,
-  moves: ["Quick Attack", "Thunderbolt", "Tail Whipe", "Growl"],
-  weaknesses: ["Ground"],
-};
-export const data = [chamanderData, squirtleData, bulbasaurData, pikatchuData];
+export const data = [
+  {
+    name: "Pikatchu",
+    image: require("./assets/pikachu.png"),
+    type: "Electric",
+    hp: 35,
+    moves: ["Quick Attack", "Thunderbolt", "Tail Whipe", "Growl"],
+    weaknesses: ["Ground"],
+  },
+  {
+    name: "Bulbasaur",
+    image: require("./assets/bulbasaur.png"),
+    type: "Grass",
+    hp: 45,
+    moves: ["Tackle", "Vine Whipe", "Growl", "Leech Seed"],
+    weaknesses: ["Fire", "Ice", "Flaying", "Psychic"],
+  },
+  {
+    name: "Squirtle",
+    image: require("./assets/squirtle.png"),
+    type: "Water",
+    hp: 44,
+    moves: ["Tackle", "Water Gun", "Tail", "Withdraw"],
+    weaknesses: ["Electric", "Grass"],
+  },
+  {
+    name: "Chamander",
+    image: require("./assets/charmander.png"),
+    type: "Fire",
+    hp: 39,
+    moves: ["Scratch", "Ember", "Growl", "Leer"],
+    weaknesses: ["Water", "Rock"],
+  },
+];
